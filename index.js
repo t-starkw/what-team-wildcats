fc4e3a431f1adcf1092fadce5b43fedefd84eac9
tessa <tess.starkweather@gmail.com>
2022-11-20 19:01:23 -0500

    psuedo-coding class constructor plan

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+// Declare depencies
 const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
+// TODO: Require lib functions (Manager, engineer, etc.)
+
+// Declare variables
+const teamMembers = [];
+const idArray = [];
+
+// Give usage prompt
+console.log(
+    '\nWelcome to the team generator!\nUse `npm run reset` to reset the dist/ folder.\n'
+)
+
+// Initiate inquirer prompts for team information
